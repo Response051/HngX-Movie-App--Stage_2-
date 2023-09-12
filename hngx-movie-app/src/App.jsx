c2e5baf129f76449e6b5579759e9aa5e6bd05100
Response051 <platoon1277@gmail.com>
2023-09-12 11:45:11 +0100

    pre Insatallation of Movie app with react-vite(swg+js) and tailwind

---
+++ hngx-movie-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import LandindPage from "./LandingPageDir/LandindPage";
+
+function App() {
+  return (
+    <>
+      <LandindPage />
+      <p className="text-blue-800">hi,this is a test run</p>
+      <p className="text-green-700">hello world!</p>
+    </>
+  );
+}
+
+export default App;
---
+++ hngx-movie-app/src/LandingPageDir/LandindPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandindPage() {
+  return (
+    <div className=''>LandindPage</div>
+  )
+}
+
+export default LandindPage;
